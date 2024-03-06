939bbf3f9502cb4ae6bbea4e0b728ff2908dd554
AbdullatefBinosseil <abdullatifff939@gmail.com>
2024-03-07 01:14:00 +0300

    I have finished part of the first view

--- theApp/views.py
+++ theApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django import forms
+people=[]
 # Create your views here.
+class person(forms.Form):
+    username=forms.CharField(label="username")
+    password=forms.PasswordInput(lable="password")
+    def __init__(self,username,password):
+        self.username=username
+        self.password=password
+
+def add(request):
+    return render(request, "theApp/add.html", {
+        "form":person()
+    })
+
+
