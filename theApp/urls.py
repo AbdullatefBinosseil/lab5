cf96c183fbf4bf8f975e0dc9861b85252aa3036a
AbdullatefBinosseil <abdullatifff939@gmail.com>
2024-03-07 23:00:55 +0300

    I added and modified some files

--- theApp/urls.py
+++ theApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name="theApp"
 urlpatterns=[
-    path("add", view.add, name="add")
+    path("add", views.add, name="add"),
+    path("", views.index, name="index")
 ]
--- theApp/views.py
+++ theApp/views.py
@@ -1,17 +1,38 @@
 from django.shortcuts import render
 from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 people=[]
 # Create your views here.
 class person(forms.Form):
     username=forms.CharField(label="username")
-    password=forms.PasswordInput(lable="password")
+    password=forms.CharField(widget=forms.PasswordInput(), label="Password")
     def __init__(self,username,password):
         self.username=username
         self.password=password
+    def __str__(self):
+        return self.username
 
 def add(request):
+    if request.method == "POST" :
+        form = person(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            Personn = person(username=username, password=password)
+            people.append(Personn)
+            return HttpResponseRedirect (reverse("theApp: index"))
+        else:
+            return render (request, "theApp/add.html",{
+                "form":form
+            })
     return render(request, "theApp/add.html", {
         "form":person()
     })
+def index(request):
+    return render(request, "theApp/index.html",{
+        "people":people
+    })
+
 
 
