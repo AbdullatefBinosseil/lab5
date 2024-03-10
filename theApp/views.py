a59abde0ac41334562389e7440ba6a149e0259a0
AbdullatefBinosseil <abdullatifff939@gmail.com>
2024-03-10 13:07:37 +0300

    add and modified some file

--- theApp/views.py
+++ theApp/views.py
@@ -2,37 +2,34 @@ from django.shortcuts import render
 from django import forms
 from django.urls import reverse
 from django.http import HttpResponseRedirect
-people=[]
-# Create your views here.
-class person(forms.Form):
-    username=forms.CharField(label="username")
-    password=forms.CharField(widget=forms.PasswordInput(), label="Password")
-    def __init__(self,username,password):
-        self.username=username
-        self.password=password
+
+people = []
+
+class Person:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
     def __str__(self):
         return self.username
 
+class PersonForm(forms.Form):
+    username = forms.CharField(label="Username")
+    password = forms.CharField(widget=forms.PasswordInput(), label="Password")
+
 def add(request):
-    if request.method == "POST" :
-        form = person(request.POST)
+    if request.method == "POST":
+        form = PersonForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password"]
-            Personn = person(username, password)
-            people.append(Personn)
-            return HttpResponseRedirect (reverse("theApp: index"))
-        else:
-            return render (request, "theApp/add.html",{
-                "form":form
-            })
-    return render(request, "theApp/add.html", {
-        "form":person
-    })
+            new_person = Person(username, password)
+            people.append(new_person)
+            return HttpResponseRedirect(reverse("theApp:index"))
+    else:
+        form = PersonForm()
+
+    return render(request, "theApp/add.html", {"form": form})
+
 def index(request):
-    return render(request, "theApp/index.html",{
-        "people":people
-    })
-
-
-
+    return render(request, "theApp/index.html", {"people": people})
